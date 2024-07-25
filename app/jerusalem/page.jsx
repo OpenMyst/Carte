ca30df280bf0edadae755ffc10ae5d111b6e103a
Hermann <hermanrazafinbdranaivo@gmail.com>
2024-07-25 08:44:18 +0300

    Adding jeruslame page

---
+++ app/jerusalem/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+const Jerusalem = () => {
+  return (
+    <div>
+        <Spline scene="https://prod.spline.design/3k6H1cbqT90axTHH/scene.splinecode" />
+    </div>
+  )
+}
+
+export default Jerusalem
