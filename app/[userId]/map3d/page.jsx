60663b0588c6cdb6c73e205081a0676923bd0b23
Hermann <hermanrazafinbdranaivo@gmail.com>
2024-08-16 10:33:35 +0300

    enlever le div non clicable

--- app/[userId]/map3d/page.jsx
+++ app/[userId]/map3d/page.jsx
@@ -430,8 +430,7 @@ const Map3DComponent = ({ params }) => {
   return (
     <div>
       <div id="map" ref={mapContainer} />
-      <div className={`map-overlay top w-[20vw]`}>
-        <div className="w-16 h-16 m-0 mb-1 bg-slate-400 cursor-pointer"></div>
+      <div className={`map-overlay top w-[20vw] mt-16`}>
         <button className="bg-[#2E2F31]/20  p-2 m-1 text-white rounded sm:block md:hidden" onClick={e => { e.preventDefault(); setOpen(!open) }}>
           <PanelTopOpen className="text-black" />
         </button>
--- app/map2d/page.jsx
+++ app/map2d/page.jsx
@@ -200,8 +200,7 @@ export default function Map2DByUserId({ params }) {
   return (
     <main className="m-2">
       <div id="map" ref={mapContainer}></div>
-      <div className={`map-overlay top w-[20vw]`}>
-        <div className="w-16 h-16 m-0 mb-1 bg-slate-400 cursor-pointer"></div>
+      <div className={`map-overlay top w-[20vw] mt-16`}>
         <button className="bg-[#2E2F31]/20  p-2 m-1 text-white rounded sm:block md:hidden" onClick={e => { e.preventDefault(); setOpen(!open) }}>
           <PanelTopOpen className="text-black"/>
         </button>
