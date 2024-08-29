170be9d4a27c316c5dad37066099f45986468423
Hermann <65443014+Herman-Riah19@users.noreply.github.com>
2024-08-29 16:19:09 +0300

    Update page.jsx

--- app/[userId]/map3d/page.jsx
+++ app/[userId]/map3d/page.jsx
@@ -517,11 +517,11 @@ export default function MapByUserId({ params }) {
               {showMap3D ? "2D" : "3D"}
             </Button>
           </fieldset>
-          <fieldset>
+          {/*<fieldset>
             <Button variant="outlined" className={`${canAddEvent ? "text-primary" : "text-white"} font-bold`} onClick={handleOpenFormulaire}>
               <Plus />
             </Button>
-          </fieldset>
+          </fieldset>*/}
         </div>
       </div>
     </main>
