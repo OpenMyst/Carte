74049a10f6f1e9d90d935b751f5ffa300ec5f7bd
Hermann <hermanrazafinbdranaivo@gmail.com>
2024-09-03 21:28:34 +0300

    ajouter une ville sur chaque validation de map

--- tool/service.js
+++ tool/service.js
@@ -2,6 +2,9 @@
 import { collection, query, where, getDocs, addDoc, updateDoc, doc } from "firebase/firestore";
 import { database } from "@/tool/firebase";
 import mapboxgl from 'mapbox-gl';
+import { MAPBOX_TOKEN } from "@/tool/security";
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
 
 /**
  * Retrieves the first event associated with a given user's play locations.
@@ -56,7 +59,7 @@ export const addMarkerEvent = (map, userId, event) => {
   locationInput.type = 'text';
   locationInput.className = 'w-full p-2 border border-gray-300 rounded-sm';
   locationInput.placeholder = 'Enter location name';
-  
+
   const saveEventButton = document.createElement('button');
   saveEventButton.className = "w-full bg-[#ff4d00] rounded-sm text-white m-1";
   saveEventButton.innerText = 'Validate';
@@ -91,15 +94,15 @@ export const addMarkerEvent = (map, userId, event) => {
   popupContent.appendChild(divEventCreate);
 
   const popup = new mapboxgl.Popup().setDOMContent(popupContent)
-  .on('open', () => {
-    // Augmenter la taille de la croix de fermeture du popup
-    const closeButton = popup.getElement().querySelector('.mapboxgl-popup-close-button');
-    if (closeButton) {
-      closeButton.style.fontSize = '30px'; // Augmenter la taille de la croix
-      closeButton.style.width = '30px'; // Augmenter la taille de la zone cliquable
-      closeButton.style.height = '30px';
-    }
-  });
+    .on('open', () => {
+      // Augmenter la taille de la croix de fermeture du popup
+      const closeButton = popup.getElement().querySelector('.mapboxgl-popup-close-button');
+      if (closeButton) {
+        closeButton.style.fontSize = '30px'; // Augmenter la taille de la croix
+        closeButton.style.width = '30px'; // Augmenter la taille de la zone cliquable
+        closeButton.style.height = '30px';
+      }
+    });
 
   // Create a new marker and add it to the map
   const marker = new mapboxgl.Marker({ color: 'red', draggable: true })
@@ -108,7 +111,7 @@ export const addMarkerEvent = (map, userId, event) => {
     .addTo(map)
     .togglePopup();
 
-  marker.on('dragend', () =>  {
+  marker.on('dragend', () => {
     const lngLat = marker.getLngLat();
     coordinates = lngLat;
     console.log(coordinates)
@@ -127,35 +130,41 @@ export const addMarkerEvent = (map, userId, event) => {
 }
 
 let marker;
+
 /**
  * Adds a new marker to the map on click, saves its coordinates to Firestore,
  * and associates the event with the user.
  * 
  * @param {object} map - The Mapbox GL JS map instance.
  * @param {string} userId - The ID of the user for whom to save the event.
+ * @param {number[]} event - Coordinates [lng, lat] where the marker should be placed.
  */
 export const addMarkerEventInCenter = (map, userId, event) => {
-  // console.log(event);
-  let coordinates = event;
-  console.log(coordinates);
-  
   const popupContent = document.createElement('div');
-  popupContent.className = 'h-[200px] w-[200px] static';
+  popupContent.className = 'h-[125px] w-[200px] static';
 
   const popupTitle = document.createElement('h4');
-  popupTitle.className = "text-sm text-center";
-  popupTitle.innerText = "Deplacer et Zoomer sur la carte jusqu'à ce que le marqueur soit à l'endroite précis où vous souhaiter ajouter le lieu, puis cliquez sur:"
+  popupTitle.className = "text-sm text-center mt-2";
+  popupTitle.innerText = "Déplacez et zoomez sur la carte jusqu'à ce que le marqueur soit à l'endroit précis où vous souhaitez ajouter le lieu, puis cliquez sur:";
 
-  
   const saveEventButton = document.createElement('button');
-  saveEventButton.className = "w-full bg-[#ff4d00] rounded-sm text-white m-1 border-2 border-black";
+  saveEventButton.className = "w-full bg-primary rounded-sm text-white m-1 border-2 border-black";
   saveEventButton.innerText = 'Valider la position';
   saveEventButton.onclick = async () => {
-      console.log('Coordinates:', coordinates);
-      await saveCoordonneEvent(userId, coordinates); // Ajouter locationName aux paramètres de la fonction
-      // Fermer le popup
-      marker.remove();
-    };
+    if (marker) {
+      const lngLat = marker.getLngLat();
+      const locationName = await getCityName(lngLat.lng, lngLat.lat);
+      if (locationName) {
+        console.log('Coordinates:', [lngLat.lng, lngLat.lat]);
+        await saveCoordonneEvent( [lngLat.lng, lngLat.lat], locationName); // Ajouter locationName aux paramètres de la fonction
+        // Fermer le popup
+        marker.remove();
+        marker = null; // Réinitialiser le marqueur après suppression
+      } else {
+        alert('Please enter a location name.');
+      }
+    }
+  };
 
   const divEventCreate = document.createElement('div');
   divEventCreate.className = "flex gap-1";
@@ -165,37 +174,49 @@ export const addMarkerEventInCenter = (map, userId, event) => {
   popupContent.appendChild(divEventCreate);
 
   const popup = new mapboxgl.Popup().setDOMContent(popupContent)
-  .on('open', () => {
-    // Augmenter la taille de la croix de fermeture du popup
-    const closeButton = popup.getElement().querySelector('.mapboxgl-popup-close-button');
-    if (closeButton) {
-      closeButton.style.fontSize = '30px'; // Augmenter la taille de la croix
-      closeButton.style.width = '30px'; // Augmenter la taille de la zone cliquable
-      closeButton.style.height = '30px';
+    .on('open', () => {
+      const closeButton = popup.getElement().querySelector('.mapboxgl-popup-close-button');
+      if (closeButton) {
+        closeButton.style.fontSize = '30px'; // Augmenter la taille de la croix
+        closeButton.style.width = '30px'; // Augmenter la taille de la zone cliquable
+        closeButton.style.height = '30px';
 
-      closeButton.onclick = () => {
-        marker.remove();
-      };
-    }
-  });
+        closeButton.onclick = () => {
+          marker.remove();
+          marker = null; // Réinitialiser le marqueur après suppression
+        };
+      }
+    });
 
-  if (coordinates != [NaN, NaN]) {
-    if (!marker) {
-      marker = new mapboxgl.Marker({ color: 'red', draggable: true })
-        .setLngLat(coordinates)
-        .setPopup(popup)
-        .addTo(map)
-        .togglePopup();
-    } else {
-      marker.setLngLat(coordinates);
-    }
+  if (!marker) {
+    marker = new mapboxgl.Marker({ color: 'red', draggable: true })
+      .setLngLat(event)
+      .setPopup(popup)
+      .addTo(map)
+      .togglePopup();
+  } else {
+    marker.setLngLat(event);
+    marker.setPopup(popup).togglePopup();
   }
-  
-  // marker.on('dragend', () =>  {
-  //   const lngLat = marker.getLngLat();
-  //   coordinates = lngLat;
-  //   console.log(coordinates)
-  // });
+
+  // Update coordinates when the marker is dragged
+  marker.on('dragend', () => {
+    const lngLat = marker.getLngLat();
+    console.log('Updated coordinates:', [lngLat.lng, lngLat.lat]);
+  });
+}
+
+export const getCityName = async (longitude, latitude) => {
+  try {
+    const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxgl.accessToken}`);
+    const data = await response.json();
+    const placeName = data.features[0].place_name;
+
+    return placeName;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
 }
 
 /**
@@ -205,11 +226,12 @@ export const addMarkerEventInCenter = (map, userId, event) => {
  * @param {object} coordinates - The coordinates of the event (lngLat object).
  * @param {string} place - The name of the place locating
  */
-export const saveCoordonneEvent = async (userId, coordinates) => {
+export const saveCoordonneEvent = async (coordinates, place) => {
   try {
     console.log(coordinates)
     // Save the coordinates to the Firestore `events` collection
     const eventDocRef = await addDoc(collection(database, 'ville'), {
+      ville: place,
       longitude: coordinates[0],
       latitude: coordinates[1],
       etat: 0
@@ -240,20 +262,20 @@ export const createUserOpenFormulaire = async (userId) => {
       const docId = hasCreateUser.docs[0].id; // Récupère l'ID du document existant
       const hasCliqued = hasCreateUser.docs[0].data().iscliked;
       const docRef = doc(database, 'clicked', docId);
-    
+
       await updateDoc(docRef, {
         iscliked: !hasCliqued
       });
-    
+
       console.log('Document mis à jour avec succès');
     } else {
       const addFormulaire = await addDoc(
         collection(database, 'clicked'), {
-          userId: userId,
-          iscliked: true
-        }
+        userId: userId,
+        iscliked: true
+      }
       );
-    
+
       console.log('Nouveau document créé avec succès');
     }
   } catch (error) {
